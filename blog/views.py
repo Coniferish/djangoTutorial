cc34df1e44b33ded6f349cac280e49cfba7baef9
Coniferish <johnjennings702@gmail.com>
2023-02-15 14:12:02 -0500

    feat: Create initial home page html and related views with dummy data

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Buggs Bunny',
+        'title': 'Carrots?',
+        'content': 'Carrots are great.',
+        'date_posted': '20230215',
+    },
+    {
+        'author': 'Tommy Pickles',
+        'title': 'Crayons?',
+        'content': 'Crayons are great.',
+        'date_posted': '20230214',
+    }
+]
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return HttpResponse('<h1>About</h1>')
