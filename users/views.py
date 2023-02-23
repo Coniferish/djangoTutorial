9c44cda1ff8f40c862392d76960cbd6d751b6935
Coniferish <johnjennings702@gmail.com>
2023-02-23 11:57:45 -0500

    feat: Registration page
    Init register page and view using django's auth form

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+# https://docs.djangoproject.com/en/4.1/topics/auth/
+# https://docs.djangoproject.com/en/4.1/topics/auth/default/#module-django.contrib.auth.forms
+
+def register(request):
+    form = UserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
