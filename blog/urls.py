15189dc0fa37bee828edcef4a8bf9ad9e49583ea
Coniferish <johnjennings702@gmail.com>
2023-03-03 10:24:10 -0600

    feat: Add registration link to navbar
    - Create new nav-item in basetemplate
    
    - Add urlpattern for register page to blog/urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from . import views
+from . import views as blog_views
+import users.views as user_views
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
-    path('about/', views.about, name='blog-about'),
+    path('', blog_views.home, name='blog-home'),
+    path('about/', blog_views.about, name='blog-about'),
+    path('register/', user_views.register, name='blog-register')
 ]
