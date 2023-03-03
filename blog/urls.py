ef30cc06fa8a36186e015bc6984cc95c829c1578
Coniferish <johnjennings702@gmail.com>
2023-03-03 10:56:44 -0600

    feat: Login page
    - Create login page html
    
    - Add navigation between login and registration pages
    
    - Refactor url paths

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ import users.views as user_views
 urlpatterns = [
     path('', blog_views.home, name='blog-home'),
     path('about/', blog_views.about, name='blog-about'),
-    path('register/', user_views.register, name='blog-register')
+    path('register/', user_views.register, name='user-register')
 ]
