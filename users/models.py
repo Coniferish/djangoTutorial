bbbef88142c66ce0b0acd8d6f839d751fa2efb67
Coniferish <johnjennings702@gmail.com>
2023-03-08 11:36:00 -0600

    feat: Resize image if greater than 300x300

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -8,3 +9,13 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
 
+    def save(self): # the save method is automatically run whenever the model is saved,
+        # but we are adding features, so will need to access it ourselves
+        super().save()
+
+        img = Image.open(self.image.path)
+        
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
