041c55cc5b32dc27c5e5360500f3c4ac29ad5e99
Coniferish <johnjennings702@gmail.com>
2023-03-09 10:39:42 -0600

    feat: Create individual blog post view
    - Create class view
    
    - Create html
    
    - Add view to blog's urls
    
    - Update comments
    
    - Fix typo in home.html

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views as blog_views
 import users.views as user_views
-from .views import PostListView
+from .views import PostListView, PostDetailView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
+    # using a variable in the route (for individual posts, which will be numbered)
+    # the detail view is expecting the "pk" variable (we could change this in the class)
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('about/', blog_views.about, name='blog-about'),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 def home(request):
@@ -9,15 +9,23 @@ def home(request):
     }
     return render(request, 'blog/home.html', context)
 
-# class based views look for templates with a certain naming pattern:
-# <app>/<model>_<viewtype>.html
-# but we can change this default with "template_name"
+
+# https://docs.djangoproject.com/en/4.1/topics/class-based-views/
+# https://docs.djangoproject.com/en/4.1/ref/class-based-views/generic-display/#listview
 class PostListView(ListView):
+    # class views generally cut down on code and consist of simply defining attributes (more simple)
     model = Post
+    # class based views look for templates with a certain naming pattern:
+    # <app>/<model>_<viewtype>.html
     template_name = 'blog/home.html'
     # this class view also needs to know the variable name for the items it will be iterating over
     context_object_name = 'posts'
     ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    # for this class view, we're using the django defaults, 
+    # so we aren't defining the template_name, context_object_name, etc.
+    model = Post
 
 def about(request):
     return render(request, 'blog/about.html', {'title':'About'})
