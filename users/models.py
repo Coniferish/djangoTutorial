6e3414c2ddfe3abee9489b8275f21493780b9b83
Coniferish <johnjennings702@gmail.com>
2023-04-25 18:31:44 -0500

    bug: Remove picture save method because PIL doesn't work on S3

--- users/models.py
+++ users/models.py
@@ -9,21 +9,23 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
 
-    def save(self, *args, **kwargs): # the save method is automatically run whenever the model is saved,
-        # but we are adding features, so will need to access it ourselves
-        super().save(*args, **kwargs)
+    # TODO: Create AWS Lambda function that resizes our images for us since PIL won't 
+    # work properly in S3
+    # def save(self, *args, **kwargs): # the save method is automatically run whenever the model is saved,
+    #     # but we are adding features, so will need to access it ourselves
+    #     super().save(*args, **kwargs)
 
-        img = Image.open(self.image.path)
+    #     img = Image.open(self.image.path)
         
-        if img.height > 250 or img.width > 250:
-            output_size = (250, 250)
-            if img.height > img.width:
-                diff = img.height - img.width
-                #crop(left, upper, right, lower)
-                img = img.crop((0, diff/2, img.width, img.height-diff/2))
-            else:
-                diff = img.width - img.height
-                img = img.crop((diff/2, 0, img.width-diff/2, img.height))
+    #     if img.height > 250 or img.width > 250:
+    #         output_size = (250, 250)
+    #         if img.height > img.width:
+    #             diff = img.height - img.width
+    #             #crop(left, upper, right, lower)
+    #             img = img.crop((0, diff/2, img.width, img.height-diff/2))
+    #         else:
+    #             diff = img.width - img.height
+    #             img = img.crop((diff/2, 0, img.width-diff/2, img.height))
             
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+    #         img.thumbnail(output_size)
+    #         img.save(self.image.path)
