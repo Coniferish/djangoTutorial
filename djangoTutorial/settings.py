f9a9c89fe2fbe168b28562c48b88d87268ad0f14
Coniferish <johnjennings702@gmail.com>
2023-04-27 12:50:49 -0500

    Change secret key and create env var

--- djangoTutorial/settings.py
+++ djangoTutorial/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t#t=8ak#4%=t^dr_xx5=@q1$nm#+s1_6$q(zgr(@c)zgvju*kf'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djangotutorialspork.herokuapp.com']
 
 
 # Application definition
